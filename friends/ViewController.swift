d62c05ceaf77c2ea38b4e5feb9a554a1ec7a6552
Lucas Alves Da Silva <lukas-r8@lucass-macbook-pro.local>
2018-08-01 22:53:21 +0300

    Initial Commit

---
+++ friends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  friends
+//
+//  Created by Lucas Alves Da Silva on 01/08/2018.
+//  Copyright © 2018 Lucas Alves Da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
